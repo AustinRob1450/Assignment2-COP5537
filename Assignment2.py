0273773454e8d8f6c58e9fce3d30b0517205e91c
AustinRob1450 <53237132+AustinRob1450@users.noreply.github.com>
2022-10-11 21:37:57 -0400

    FIRSTY

---
+++ Assignment2.py
@@ -0,0 +1,35 @@
+import pandas
+import sys
+import scipy
+
+class Fleury():
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = {}
+
+
+    
+    isValidEdge(self, point, vertex, visited)
+    printGraph(self)
+    primsCheck(self, vertextoDelete)
+    # Check if the verticies have an even degree.
+    checkDegrees(self)
+    # Probably to make the graph even.
+    def addEdge(self, currV, newVertex):
+        # Adding each other to each others list so we can cross-verify they are touching.
+        self.graph[current].append(newVertex)
+        self.graph[newVertex].append(current)
+    
+    # Also means breaking apart the vertices to see where we can remove and still have a good Euler graph
+    # Find the vertices pair in the graph, mark it as -1 in their respective lists.
+    def removeEdge(self, currV, adjV):
+        # Uses tuple structure to tack what V we are at and what we are iterating towards (the key Vertex to delete)
+        for itr in self.graph[iter]:
+
+
+
+
+if __name__ == '__main__':
+    # Read in the text file here.
+    
+    print("I tried")
