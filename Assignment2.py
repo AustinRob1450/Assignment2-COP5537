04bc50257b225f63a83ab56c8aad666f5cb53dcc
AustinRob <53237132+AustinRob1450@users.noreply.github.com>
2022-10-11 23:58:45 -0400

    Good progress - most methods done

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,6 @@
+import enum
+from re import T
+from tracemalloc import start
 import pandas
 import sys
 import scipy
@@ -7,29 +10,93 @@ class Fleury():
         self.V = vertices
         self.graph = {}
 
+    # ---------- addEdge
+    # ----------------------------------------
 
-    
-    isValidEdge(self, point, vertex, visited)
-    printGraph(self)
-    primsCheck(self, vertextoDelete)
-    # Check if the verticies have an even degree.
-    checkDegrees(self)
     # Probably to make the graph even.
-    def addEdge(self, currV, newVertex):
+    def addEdge(self, currentV, newVertex):
         # Adding each other to each others list so we can cross-verify they are touching.
-        self.graph[current].append(newVertex)
-        self.graph[newVertex].append(current)
+        self.graph[currentV].append(newVertex)
+        self.graph[newVertex].append(currentV)
     
+
+    # ---------- removeEdge
+    # ----------------------------------------
+
     # Also means breaking apart the vertices to see where we can remove and still have a good Euler graph
     # Find the vertices pair in the graph, mark it as -1 in their respective lists.
-    def removeEdge(self, currV, adjV):
+    # ---------- IS THIS PRIMS? ------------
+    def removeEdge(self, currV, pairedV):
         # Uses tuple structure to tack what V we are at and what we are iterating towards (the key Vertex to delete)
-        for itr in self.graph[iter]:
+        for itr, adjacentV in enumerate(self.graph[currV]):
+            if adjacentV == pairedV:
+                self.graph[currV].pop(itr)
+        for itr, adjacentV in enumerate(self.graph(pairedV)):
+            if adjacentV == currV:
+                self.graph[pairedV].pop(itr)
+    
+
+    # ---------- isBridge
+    # ----------------------------------------
+    
+    # Is adjacnecy measured in connections to a vertex?
+    ## DOUBLE CHECK
+    def isBridge(self, currentV):
+        degree = 0
+        for itr in enumerate(self.graph[currentV]):
+            degree += 1
+            if (degree > 1):
+                return False
+        return True
 
 
+    # ---------- findStart
+    # ----------------------------------------
 
+    # If there are no odd degree vertices we can pick any V as the start.
+    # If there is an odd degree vertices we have to pick it.
+    def findStart(self):
+        degree = 0
+        for itr in enumerate(self.graph):
+
+            for atr in enumerate(self.graph):
+                degree += 1
+
+            if degree % 2 != 0:
+                return itr
+        return 0
+    
+
+    # ---------- depthFirstSearch
+    # ----------------------------------------
+
+    # We need a list of visted vertices in order to perform DFS
+    def depthFirstSearch(self, currentV, visited):
+        visited[currentV] = True
+        count=1
+        for itr in self.graph[currentV]:
+            if visited[itr] == False:
+                count += count + self.depthFirstSearch(itr, visited)
+        return count
+    
+
+    # ---------- checkEdge
+    # ----------------------------------------
+
+    # True in Two conditions: 
+    # 1.) One one adj vertex of currentV. 
+    # 2.) Not a bridge, etc.
+    def checkEdge(self, currentV, pairedV):
+        visited= False * self.V
+        # Only 1 other vertex adjacent
+        if len(self.graph[currentV] == 1):
+            return True
+        counter = self.depthFirstSearch(currentV, visited)
+        self.removeEdge(currentV, pairedV)
+        
+
+        # dpethFirstSearch
 
 if __name__ == '__main__':
     # Read in the text file here.
-    
     print("I tried")
